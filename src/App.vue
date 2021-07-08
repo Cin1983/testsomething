c01a1e3e4529fc1927f66f85507017ffb5bc4311
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-07-08 15:04:23 +0200

    Button-wrapper

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="button-wrapper">
+        <button class="btn" @click="searchUnsplash('Cars')">CARS</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
